71477a24083883fb9f95db8b505515095f9bfc40
oh no, anyways <78188625+paulopoig@users.noreply.github.com>
2021-06-20 01:54:57 +0800

    Register Update :DD with plan.
    Help <3

--- scripts/register.js
+++ scripts/register.js
@@ -6,20 +6,20 @@ console.log("HEHEH");
     var firstname = document.getElementById("_firstname").value;
     var middlename = document.getElementById("_middlename").value;
     var lastname = document.getElementById("_lastname").value;
+    var studentNumber = "";
 
-function selectCourse(){
     
-}
-
-
     function getButton(){
         const xhttp = new XMLHttpRequest();
     
         xhttp.onload = function() {
             var content = "";
             var obj = JSON.parse(this.responseText);
-            console.log(obj.message.first_name);
-            document.getElementById("he").innerHTML= obj.message.first_name;
+            console.log(obj);
+            
+
+            document.getElementById("he").innerHTML= obj.message.last_name;
+            
         }
     
         // Send a request
@@ -27,28 +27,61 @@ function selectCourse(){
         xhttp.send();
     }
 
-function validateForms(){
-    if(email == "" && password == "" && firstname == "" && middlename == "" && lastname == ""){
-        alert("Name must be filled out");
-    }
-    else{
-        enrollStudent();
-    }
-  }
-
-function enrollStudent(){
+// function validateForms(){
+//     if(email == "" && password == "" && firstname == "" && middlename == "" && lastname == ""){
+//         alert("Name must be filled out");
+//     }
+//     else{
+//         enrollStudent();
+//     }
+//   }
     
-        // Create an XMLHttpRequest object
-        const xhttp = new XMLHttpRequest();
+
+    function enrollStudent(){
+   
+    var course = document.getElementById("course");
+    var strUser = course.value;
+        console.log(strUser);
+       
+        var studentNumber = "";
+        var courseCode = "";
+        var subjects = [];
+
+    if(strUser == "BSIT"){
+        
+        courseCode = "BSIT"
+        subjects = ["PED 0012", "EIT 0121.1", "PCM 0006", "IPP 0010", "CET 0121",
+         "NSTP 02", "EIT 0122", "ICC 0103.1", "ICC 0103"]
+
+    } else if(strUser == "BSCE"){
+        courseCode = "BSCE"
+        subjects = ["PED 0012", "EIT 0121.1", "PCM 0006", "IPP 0010", "CIV 0121.1",
+    "CET 0122A", "CET 0121", "NSTP 02", "EIT 0122"]
+    } 
+    
+    else {
+        courseCode = "BSCS"
+        subjects = ["PED 0012", "EIT 0121.1", "PCM 0006", "IPP 0010",
+     "CET 0121", "NSTP 02", "EIT 0122", "ICC 0103.1", "ICC 0103"]
+    };
+      
+       
+    
+    //  Create an XMLHttpRequest object
+       const xhttp = new XMLHttpRequest();
 
         // Define a callback function
         xhttp.onload = function() {
             var content = "";
             var obj = JSON.parse(this.responseText);
-            console.log(obj);
-   
+            console.log(obj.message.first_name);
+            
+            var data = obj.message;
+            studentNumber = data.student_number;
+            console.log(studentNumber);
+
             if (obj.status_code == "SUCCESS"){
-                    
+                // window.location = "../Webpage/mainpage.html";
                 }
                 else{
     
@@ -60,36 +93,38 @@ function enrollStudent(){
 
             
     
-            // Send a request with parameters
-            xhttp.open("POST", "http://localhost:8000/user/create", true);
-            xhttp.setRequestHeader('Content-Type', 'application/json');
-            var params = {
-                email: email,
-                password: password,
-                first_name: firstname,
-                middle_name: middlename,
-                last_name: lastname,
-                is_student: defaultValue,
-                year_level: defaultValue
-            };
-            xhttp.send(JSON.stringify(params));
+//             // Send a request with parameters
+//             xhttp.open("POST", "http://localhost:8000/user/create", true);
+//             xhttp.setRequestHeader('Content-Type', 'application/json');
+//             var params = {
+//                 email: email,
+//                 password: password,
+//                 first_name: firstname,
+//                 middle_name: middlename,
+//                 last_name: lastname,
+//                 is_student: defaultValue,
+//                 year_level: defaultValue
+//             };
+//             xhttp.send(JSON.stringify(params));
+          
 
+            xhttp.open("GET", "http://localhost:8000/user/2021-00001", true);
+            xhttp.send();
             
-
+           
             xhttp.open("POST", "http://localhost:8000/enrollment/create", true);
             xhttp.setRequestHeader('Content-Type', 'application/json');
             var enrollParams = {
-                email: email,
-                password: password,
-                first_name: firstname,
-                middle_name: middlename,
-                last_name: lastname,
-                is_student: defaultValue,
-                year_level: defaultValue
+                student_number: obj.message.student_number,
+                course_code: courseCode,
+                subjects: subjects
             };
             xhttp.send(JSON.stringify(enrollParams));
+            
+        
+           // window.location = "../Webpage/mainpage.html";
 
-            window.location = "../Webpage/mainpage.html";
-}
+            
+ }
 
 
